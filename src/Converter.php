070f5d981c10cd7cbd8761fb0ad4e1808b06a6f4
Marisa Clardy <marisa@clardy.eu>
2021-01-23 18:37:35 -0600

    Setup initial test.

---
+++ src/Converter.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GCSS;
+
+class Convertor
+{
+    public function execute(string $input): string
+    {
+        return '';
+    }
+}
---
+++ tests/Integration/ConvertGCSSTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GCSS\Tests\Integration;
+
+use GCSS\Convertor;
+use PHPUnit\Framework\TestCase;
+
+class ConvertGCSSTest extends TestCase
+{
+    /** @test */
+    public function test_conversion_of_gcss_to_tres()
+    {
+        $this->markTestIncomplete('Implementation is far from complete.');
+        
+        $input = file_get_contents(__DIR__ . '/../fixtures/theme.gcss');
+        $convertor = new Convertor();
+        $this->assertStringEqualsFile(__DIR__ . '/../fixtures/theme.gcss', $convertor->execute($input));
+    }
+}
