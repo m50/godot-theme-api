7d5a4d96315d9c70b61281bb764d0d5035a1846a
Marisa Clardy <marisa@clardy.eu>
2021-01-23 19:54:31 -0600

    Some more setup.

---
+++ src/Endpoint.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GCSS;
+
+use Exception;
+
+class Endpoint
+{
+    public function __invoke()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            echo '<p style="font-size: 4rem">This end point is only accessible via POST. <br />';
+            echo 'Please visit <a href="https://github.com/m50/godot-theme-api">the github repository</a> to learn more.</p>';
+            die();
+        }
+
+        $postInput = file_get_contents('php://input');
+
+        $converter = new Converter();
+        try {
+            $tresText = $converter->execute($postInput);
+            header("Content-Type: text/plain");
+            echo $tresText;
+        } catch (Exception $e) {
+            header("Content-Type: text/plain");
+            http_response_code(500);
+            echo $e->getMessage() . PHP_EOL;
+            echo $e->getTrace() . PHP_EOL;
+        }
+    }
+}
